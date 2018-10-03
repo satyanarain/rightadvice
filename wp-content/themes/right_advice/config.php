a7902015010d1eb45e9640187ec8b1dd3fe6711e
Opiant <assets@opiant.online>
2018-10-03 18:21:04 +0530

    modified

--- wp-content/themes/right_advice/config.php
+++ wp-content/themes/right_advice/config.php
@@ -1,11 +1,11 @@
 <?php 
 define('DB_NAME', 'curedvtv_rightadvice_new');
-define('DB_USER', 'curedvtv_right');
-define('DB_PASSWORD', '59T(I,*M0%)S');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'satya@1234');
 define('DB_HOST', 'localhost');
 
-$conn = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
-$db = mysql_select_db(DB_NAME,$conn);
+$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
+$db = mysqli_select_db(DB_NAME,$conn);
 if(!$db)
 	echo "Database connection error";
 
