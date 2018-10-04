bc7f96f853d071fe5de61949492b6569cf6d0b7c
Opiant <assets@opiant.online>
2018-10-04 10:26:18 +0530

    m

--- wp-content/themes/right_advice/config.php
+++ wp-content/themes/right_advice/config.php
@@ -1,20 +1,22 @@
 <?php 
-/* 
-define('DB_NAME', 'curedvtv_rightadvice_newcuredvtv_rightadvice_new');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root@1234');
-define('DB_HOST', 'localhost');
-$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
-$db = mysqli_select_db(DB_NAME,$conn);
-if(!$db)
-        echo "Database connection error";
-*/
 $con = mysqli_connect("localhost","root","root@1234","curedvtv_rightadvice_new");
+$servername = "localhost";
+$database = "curedvtv_rightadvice_new";
+$username = "root";
+$password = "root@1234";
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+echo "Connected successfully";
+mysqli_close($conn);
 
 ?>
