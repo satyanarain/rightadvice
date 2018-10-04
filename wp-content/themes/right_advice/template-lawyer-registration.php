ae95229b91a15ef4fca9fb1da920df1e6ab7ee73
Opiant <assets@opiant.online>
2018-10-04 10:42:45 +0530

    m

--- wp-content/themes/right_advice/template-lawyer-registration.php
+++ wp-content/themes/right_advice/template-lawyer-registration.php
@@ -117,13 +117,14 @@ include("config.php");
 			<div class="col-md-4 inputGroupContainer">
 				<div class="input-group">
 					<?php
-					   $sql = mysqli_query("SELECT * FROM apps_countries") or die(mysqli_error());
+					   $sql = "SELECT * FROM apps_countries";
+                                           $res=mysqli_query($conn, $sql) or die(mysqli_error());
 					?>
 					<span class="input-group-addon"><i class="fa fa-address-card" requiredaria-hidden="true"></i></span>
 					<select name="country" id="country" required class="form-control"><?php if(isset($_SESSION['country'])){ echo $_SESSION['country']; }?>
 					    <option value="" selected>Select Country</option>
 					    <?php
-					    while($country = mysqli_fetch_array($sql))
+					    while($country = mysqli_fetch_array($res))
 					{ ?>
 						<option value="<?php echo $country['country_name']; ?>" <?php if(isset($_SESSION['country']) && $_SESSION['country'] == $country['country_name']) echo 'selected';?>><?php echo $country['country_name']; ?></option>
 					<?php } ?>
